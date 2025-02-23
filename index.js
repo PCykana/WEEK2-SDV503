5b511aa31f21141e9d99d67aab60a0c19865f3df
PCykana <peter-cykana@live.nmit.ac.nz>
2025-02-24 11:22:33 +1300

    new js code

--- index.js
+++ index.js
@@ -3,4 +3,23 @@
 /** multi - line text
  * use this kind of thig for intros and overviews of text
  */
-console.log(2+1)
+//console.log(2+1)
+//ES5 
+var a = 2
+
+//ES6
+const b = 3
+let c = 4
+
+//operators +, -, /, and *
+console.log (a-c)
+
+let num1 = 5
+let num2 = 10
+const result =num1 * num2
+console.log("Result= ",result)
+
+let num3 = 42
+let num4 = 24
+const res2 = num3 / num4
+console.log("Result 2= ", res2)
